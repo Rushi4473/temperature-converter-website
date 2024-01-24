1187628a0d81043b03535db638ceecbf5bddbb4b
Rushikesh Gaikwad <147429159+Rushi4473@users.noreply.github.com>
2024-01-24 08:12:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function convert()
+{
+    let temp = document.getElementById("input").value;
+    let type = document.getElementById("type").value;
+    if (type == "fahrenheit")
+    {
+        let celcius = (temp - 32) / 1.8;
+        let result = celcius.toFixed(4);
+        document.getElementById("result").innerText = result + " ℃";
+    }
+    else if (type == "celcius")
+    {
+        let fahrenheit = (temp * 1.8) + 32;
+        let result = fahrenheit.toFixed(4);
+        document.getElementById("result").innerText = result + " ℉";
+    }
+}
